127de7554533cbe0a0d34881ee31662ad7a1662f
Palash Pingale <palashpingale135@gmail.com>
2026-05-23 14:51:55 +0530

    feat: frontend complete, full stack working locally

--- incidentiq/settings.py
+++ incidentiq/settings.py
@@ -37,7 +37,7 @@ ROOT_URLCONF = "incidentiq.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "templates", BASE_DIR / "frontend"],
         "APP_DIRS": True,
         "OPTIONS": {"context_processors": []},
     },
